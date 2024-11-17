30a6a8b283daead259e35bc988a88ba02e66087b
arpan jana <arpangroup1@gmail.com>
2024-11-18 00:56:00 +0530

    first commit

---
+++ 03-rest-api-both_provider_consumer_contract_test/src/main/java/com/arpan/__rest_api_both_provider_consumer_contract_test/Application.java
@@ -0,0 +1,13 @@
+package com.arpan.__rest_api_both_provider_consumer_contract_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
