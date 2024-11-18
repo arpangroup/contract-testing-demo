26c8835267b64959d0fd2e8c41c4f6c141d57f6f
arpan jana <arpangroup1@gmail.com>
2024-11-18 23:30:45 +0530

    initial commit

--- 04-kafka-avro-consumer/src/test/java/com/arpan/__kafka_avro_consumer/contract/StudentConsumerPactTestV1.java
+++ 04-kafka-avro-consumer/src/test/java/com/arpan/__kafka_avro_consumer/contract/StudentConsumerPactTestV1.java
@@ -9,6 +9,7 @@ import au.com.dius.pact.core.model.annotations.Pact;
 import au.com.dius.pact.core.model.messaging.Message;
 import au.com.dius.pact.core.model.messaging.MessagePact;
 import com.arpan.__kafka_avro_consumer.consumer.StudentConsumer;
+import com.arpan.__kafka_avro_consumer.utils.AvroUtil;
 import com.arpangroup.model.Student;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.Schema;
@@ -37,74 +38,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 @PactTestFor(providerName = "student-provider", providerType = ProviderType.ASYNCH, pactVersion = PactSpecVersion.V3)
 public class StudentConsumerPactTestV1 {
     private static final String AVRO_SCHEMA_PATH = "src/main/resources/avro/student.avsc";
+    private Schema schema;
 
     @InjectMocks
     private StudentConsumer studentConsumer;
 
     @Pact(consumer = "student-consumer")
     public MessagePact studentDetailsPact(MessagePactBuilder builder) throws Exception {
-        String avroMessageBase64 = createAvroMessageBase64();
+        //String avroMessageBase64 = AvroUtil.createAvroMessageBase64(AVRO_SCHEMA_PATH);
+        byte[] sampleAvroMessage = AvroUtil.getSampleAvroMessage(AVRO_SCHEMA_PATH);
+        System.out.println("#####SAMPLE_AVRO_MESSAGE: " + new String(sampleAvroMessage));
 
-        return builder
+        /*return builder
                 .expectsToReceive("a student contract in Avro format")
                 .withMetadata(Map.of("contentType", "application/avro", "encoding", "base64"))
                 .withContent(avroMessageBase64)
+                .toPact();*/
+
+        return builder
+                .expectsToReceive("a student contract in Avro format")
+                .withMetadata(Map.of("contentType", "text/plain", "encoding", "base64"))
+                .withContent(Base64.getEncoder().encodeToString(sampleAvroMessage))
                 .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "studentDetailsPact", providerType = ProviderType.ASYNCH)
     void testStudentDetailsContract(List<Message> messages) throws Exception {
-        Student expectedStudent = new Student("John Doe", "S12345", 30);
-
-        /*String payload = new String(messages.get(0).getContents().getValue());
-        Student actualStudent = new ObjectMapper().readValue(payload, Student.class);
-        assertThat(actualStudent).usingRecursiveComparison().isEqualTo(expectedStudent);*/
-
         messages.forEach(message -> {
             try {
-                assert message.getContents().getValue() != null;
-                String avroPayloadBase64 = new String(message.getContents().getValue());
-                byte[] avroPayload = Base64.getDecoder().decode(avroPayloadBase64);
-                Student actualStudent = deserializeAvroMessage(avroPayload);
-                assertThat(actualStudent).usingRecursiveComparison().isEqualTo(expectedStudent);
-            } catch (Exception e) {
+                // Get the byte array from the message content
+                byte[] avroPayload = message.getContents().getValue();
+                byte[] decodedPayload = Base64.getDecoder().decode(avroPayload);
+
+                // Deserialize the Avro message
+                Student actualStudent = AvroUtil.deserializeAvroMessage(AVRO_SCHEMA_PATH, decodedPayload);
+
+                // Validate using field-level comparison
+                assertThat(actualStudent).isNotNull();
+                assertThat(actualStudent.getStudentName()).isEqualTo("John Doe");
+                assertThat(actualStudent.getStudentId()).isEqualTo("S12345");
+                assertThat(actualStudent.getAge()).isEqualTo(30);
+            }  catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
-    }
-
-
-    private String createAvroMessageBase64() throws Exception {
-        Schema schema = new Schema.Parser().parse(new File(AVRO_SCHEMA_PATH));
-
-        GenericRecord record = new GenericData.Record(schema);
-        record.put("studentName", "John Doe");
-        record.put("studentId", "S12345");
-        record.put("age", 30);
-
-        DatumWriter<GenericRecord> writer = new SpecificDatumWriter<>(schema);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-        writer.write(record, encoder);
-        encoder.flush();
-
-        byte[] avroMessage = outputStream.toByteArray();
-        return Base64.getEncoder().encodeToString(avroMessage);
-    }
-
-
-    private Student deserializeAvroMessage(byte[] avroPayload) throws Exception {
-        Schema schema = new Schema.Parser().parse(new File(AVRO_SCHEMA_PATH));
-
-        DatumReader<GenericRecord> reader = new SpecificDatumReader<>(schema);
-        BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(avroPayload, null);
-        GenericRecord record = reader.read(null, decoder);
-
-        return new Student(
-                record.get("studentName").toString(),
-                record.get("studentId").toString(),
-                (int) record.get("age")
-        );
     }
 }
